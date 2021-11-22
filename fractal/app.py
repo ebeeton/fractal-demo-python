be01f7014e598c0be325005ad553d7836025ba6c
Evan Beeton <3587393+ebeeton@users.noreply.github.com>
2021-11-22 13:46:00 -0500

    Test image generation is working.

--- fractal/app.py
+++ fractal/app.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
 """ Fractal generation demo. Copyright (C) 2021 Evan Beeton. """
-from flask import Flask
+from io import BytesIO
+from flask import Flask, Response
+from PIL import Image
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def index():
-    return "<p>Hello, world!</p>"
+    """ Default route. """
+    return '<p>Hello, world!</p>'
+
+@app.route('/image')
+def image():
+    """ Draws a test image. """
+    size = (320, 240)
+    buf = bytearray(size[0] * size[1] * 3)
+    for i in range(0, len(buf), 3):
+        buf[i] = 255
+
+    # https://pillow.readthedocs.io/en/stable/reference/Image.html#PIL.Image.frombuffer
+    mode = 'RGB'
+    img = Image.frombuffer(mode, size, bytes(buf), 'raw', mode, 0, 1)
+    img_bytes = BytesIO()
+    img.save(img_bytes, 'PNG')
+    img_bytes.seek(0)
+    return Response(img_bytes, mimetype='image/png')
 
 if __name__ == '__main__':
     app.logger.setLevel("INFO")
--- fractal/scale.py
+++ fractal/scale.py
@@ -1,7 +1,7 @@
 """ Scaling functions. """
 
-def scale(x, minx, maxx, minscale, maxscale):
-    """ Scales a value in one range to another. """ 
+def scale(val, minx, maxx, minscale, maxscale):
+    """ Scales a value in one range to another. """
 
     # https://stackoverflow.com/a/5295202
-    return (maxscale - minscale) * (x - minx) / maxx - minx + minscale
+    return (maxscale - minscale) * (val - minx) / maxx - minx + minscale
